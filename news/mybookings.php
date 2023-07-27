25533d8b8aba74dfd4db0e499875d65bb44edc44
bipin24746 <102451706+bipin24746@users.noreply.github.com>
2023-07-27 10:19:47 +0545

    Add files via upload

---
+++ news/mybookings.php
@@ -0,0 +1,94 @@
+<?php include('header.php'); ?>
+
+<style>
+  .booking-list {
+    margin-top: 50px;
+  }
+
+  .booking-list h2 {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+
+  .booking-list table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+
+  .booking-list th,
+  .booking-list td {
+    padding: 10px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+  }
+
+  .booking-list th {
+    background-color: #f2f2f2;
+  }
+</style>
+
+<?php
+// Assuming you have already started the session to access session variables
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "moviebooking";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch the logged-in user's email address from the session variable
+$email = $_SESSION['email'];
+
+// Fetch all the bookings for the logged-in user using the email address
+$sql = "SELECT booking.*, movie.name AS movie_name FROM booking JOIN movie ON booking.movie_id = movie.id WHERE booking.email = '$email'";
+$result = $conn->query($sql);
+
+if ($result && $result->num_rows > 0) {
+    echo "<div class='booking-list'>";
+    echo "<h2>My Booking List</h2>";
+
+    echo "<table>";
+    echo "<tr>
+            <th>Name</th>
+            <th>Movie Name</th>
+            <th>Show Date</th>
+            <th>Show Time</th>
+            <th>Booking Date</th>
+            <th>Booking Time</th>
+            <th>Seat no</th>
+          </tr>";
+
+    while ($row = $result->fetch_assoc()) {
+        $fname = $row['fname'];
+        $movieName = $row['movie_name'];
+        $showDate = date('F j, Y', strtotime($row['show_date'])); // Format the date
+        $showTime = date('h:i A', strtotime($row['show_time'])); // Format the time
+        $bookingDate = date('F j, Y', strtotime($row['booking_date'])); // Format the booking date
+        $bookingTime = date('h:i A', strtotime($row['booking_time'])); // Format the booking time
+        $seatid = $row['seat_id'];
+
+        echo "<tr>
+                <td>$fname</td>
+                <td>$movieName</td>
+                <td>$showDate</td>
+                <td>$showTime</td>
+                <td>$bookingDate</td>
+                <td>$bookingTime</td>
+                <td>$seatid</td>
+              </tr>";
+    }
+
+    echo "</table>";
+    echo "</div>";
+} else {
+    echo "No bookings found.";
+}
+
+$conn->close();
+?>
